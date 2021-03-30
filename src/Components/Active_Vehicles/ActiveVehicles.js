55ba0c4ba9f0aca19a74a6b72dba54254cf53fc7
David Babayev <david.babayev@luckie.com>
2021-03-30 17:37:53 -0400

    Grid logic WIP

--- src/Components/Active_Vehicles/ActiveVehicles.js
+++ src/Components/Active_Vehicles/ActiveVehicles.js
@@ -5,22 +5,23 @@ import '../../scss/active-vehicles.scss'
 
 const ActiveVehicles = (data) => {
   let vehicleData = [];
-  if (data.data) {
-    console.log(data.data, "data");
-    vehicleData = data.data;
-    
-    console.log(vehicleData, "vdata2");
-  }
+  
+  vehicleData = data.data;
+  
 
   const setVehicleGrid = () => {
     console.log(vehicleData, "vdata");
+    if (!vehicleData.length) {
+      return 
+    }
+ 
     return vehicleData[0].map((item, index) => {
+      
       return (
         <div className={`module__active-vehicles__vehicle-block`}>
           <div className={`module__active-vehicles__vehicle-block__vehicle`}>
             {item.properties.name} has {item.properties.chargeLevel}% charge remaining
           </div>
-
         </div>
       );
     });
@@ -30,7 +31,7 @@ const ActiveVehicles = (data) => {
     <>
       <div className={"module__active-vehicles"}>
         <div className={`module__active-vehicles__header`}>
-          <h3>You have {vehicleData[0].length} active vehicles:</h3>
+          <h3>Current active vehicles:</h3>
         </div>
         <div className={`module__active-vehicles__vehicle-grid`}>
           {setVehicleGrid()}
